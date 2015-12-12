8f6e54132bf92caf474cbe715023df739a0b8e78
longloaf <maksimsoldatov@gmail.com>
2015-12-12 23:59:32 +0300

    cam

---
+++ Assets/Scripts/Cam.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cam : MonoBehaviour {
+
+	[SerializeField] Rigidbody2D target;
+	Vector2 targetPos;
+	
+	[Space(10)]
+	
+	[SerializeField] float posSmoothTime = 1;
+	Vector2 posVel = Vector2.zero;
+	
+	[Space(10)]
+	
+	[SerializeField] AnimationCurve sizeCurve;
+	[SerializeField] float sizeSmoothTime = 1;
+	float sizeVel;
+	
+	Camera cam;
+	
+	void Awake()
+	{
+		cam = GetComponent<Camera> ();
+		targetPos = target.position;
+	}
+	
+	void Update () {
+		float targetVel = 0;
+		if (target != null) {
+			targetPos = target.position;
+			targetVel = target.velocity.magnitude;
+		}
+		Vector2 newPos = Vector2.SmoothDamp (transform.position, targetPos, ref posVel, posSmoothTime);
+		transform.position = new Vector3 (newPos.x, newPos.y, transform.position.z);
+		
+		cam.orthographicSize = Mathf.SmoothDamp(cam.orthographicSize, sizeCurve.Evaluate(targetVel), ref sizeVel, sizeSmoothTime);
+	}
+}
