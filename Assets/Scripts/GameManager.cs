31d9f02f96f0fcc454641717fba710360004cd62
longloaf <maksimsoldatov@gmail.com>
2015-12-12 16:42:16 +0300

    2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float forceMag = 10;
+	public float forceAng = 45;
+
+	Rigidbody2D rb;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	Vector2 Vec2(float mag, float ang_deg)
+	{
+		float ang_rad = ang_deg * Mathf.Deg2Rad;
+		return new Vector2 (mag * Mathf.Cos (ang_rad), mag * Mathf.Sin (ang_rad));
+	}
+
+	void FixedUpdate()
+	{
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rb.AddForce(Vec2 (forceMag, forceAng), ForceMode2D.Force);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			rb.AddForce(Vec2(forceMag, 180 - forceAng), ForceMode2D.Force);
+		}
+	}
+}
