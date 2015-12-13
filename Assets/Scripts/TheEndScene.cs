1112a4766132e6e4673be1016595b4de9dd3ad37
longloaf <maksimsoldatov@gmail.com>
2015-12-13 14:23:22 +0300

    levels

---
+++ Assets/Scripts/TheEndScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheEndScene : MonoBehaviour {
+	
+	void Start () {
+		Invoke ("GotoMenu", 3);
+	}
+
+	void GotoMenu()
+	{
+		Application.LoadLevel ("Menu");
+	}
+
+}
