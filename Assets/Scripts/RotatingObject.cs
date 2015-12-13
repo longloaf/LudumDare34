7fe40249228faa56be721a0256a52a82f58d783e
longloaf <maksimsoldatov@gmail.com>
2015-12-13 23:03:23 +0300

    x

---
+++ Assets/Scripts/RotatingObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingObject : MonoBehaviour {
+
+	public float angSpeed = 180;
+	
+	void FixedUpdate()
+	{
+		transform.Rotate (0, 0, angSpeed * Time.deltaTime);
+	}
+}
