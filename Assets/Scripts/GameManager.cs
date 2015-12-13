1b189b7dfc826a3b6ab2daa649f6996ca2ed100c
longloaf <maksimsoldatov@gmail.com>
2015-12-13 17:26:42 +0300

    l

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+
+	Text progress;
 
 	Cam cam;
 
 	void Awake()
 	{
+		progress = GameObject.Find ("Progress").GetComponent<Text> ();
+		int n = Application.loadedLevel;
+		progress.text = n.ToString ("00") + "/" + (Application.levelCount - 2).ToString ("00");
+
 		cam = GameObject.Find ("Main Camera").GetComponent<Cam> ();
 	}
 	
@@ -14,10 +21,19 @@ public class GameManager : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.R)) {
 			Application.LoadLevel(Application.loadedLevel);
 		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.LoadLevel("Menu");
+		}
 	}
 
 	public void Finish()
 	{
 		cam.SetSize (cam.GetSize () / 2);
+		Invoke ("NextLevel", 2);
+	}
+
+	void NextLevel()
+	{
+		Application.LoadLevel (Application.loadedLevel + 1);
 	}
 }
